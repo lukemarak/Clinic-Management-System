e542487df2805379ce87a96c7a55c53f090381c9
lukemarak <lukemarak@gmail.com>
2025-10-31 21:59:48 +0530

    firebase credentials removed

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDoDVhF2WXVe85B90_yyPu6FDl0iyAc2nM",
-  authDomain: "clinic-management-system-9eab1.firebaseapp.com",
-  databaseURL: "https://clinic-management-system-9eab1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "clinic-management-system-9eab1",
-  storageBucket: "clinic-management-system-9eab1.firebasestorage.app",
-  messagingSenderId: "727178114385",
-  appId: "1:727178114385:web:0bbed84e3e42ed017e8f70"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_APP.firebaseapp.com",
+  databaseURL: "https://YOUR_APP.firebaseio.com",
+  projectId: "YOUR_APP",
+  storageBucket: "YOUR_APP.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
 };
-
-// Initialize Firebase only ONCE
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
